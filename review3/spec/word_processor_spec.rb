12aaddbbed23b1899b227dcca3567364da1c00a2
Julian George <jules104@gmail.com>
2019-11-02 10:24:01 +0000

    Removed test to allow updated approach

--- review3/spec/word_processor_spec.rb
+++ review3/spec/word_processor_spec.rb
@@ -1,6 +1,7 @@
 require 'word_processor'
 
 describe '#spell_checker' do
+
   it "outputs 'A' when 'A' is the input " do
     word_processor = Word_Processor.new
     expect(word_processor.spell_checker("A")).to eq("A")
@@ -8,7 +9,7 @@ describe '#spell_checker' do
 
   it "outputs 'A ~n0~' when 'A no' is the input" do
     word_processor = Word_Processor.new
-    expect(word_processor.spell_checker("A n0")).to eq("A ~n0~") 
+    expect(word_processor.spell_checker("A n0")).to eq("A ~n0~")
   end
 
 end
